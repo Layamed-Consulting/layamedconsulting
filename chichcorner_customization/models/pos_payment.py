ef0a19e75176d6d1d1bff7dbff8dbafd51e4afc6
Layamed-Consulting <mehdi.benjebara@outlook.com>
2025-01-18 22:48:56 +0100

    Dev 17-01-2025

--- chichcorner_customization/models/pos_payment.py
+++ chichcorner_customization/models/pos_payment.py
@@ -28,3 +28,25 @@ class PosPayment(models.Model):
         help='System Trace Audit Number from the associated POS order.'
     )
 
+    cheque_date = fields.Date(
+        string='Date pour le Chèque',
+        related='pos_order_id.cheque_date',
+        readonly=True,
+        help='The date associated with the cheque payment from the POS order.'
+    )
+
+    @api.depends('payment_method_id')
+    def _compute_field_visibility(self):
+        for record in self:
+            method = record.payment_method_id.name
+            record.show_stan = method == 'Carte Bancaire'
+            record.show_cheque_fields = method in ['Chèque', 'Chèque MDC']
+            record.show_date = method == 'Chèque MDC'
+            record.show_identite = method in ['Chèque MDC', 'Chèque']
+
+    show_stan = fields.Boolean(compute='_compute_field_visibility')
+    show_cheque_fields = fields.Boolean(compute='_compute_field_visibility')
+    show_date = fields.Boolean(compute='_compute_field_visibility')
+    show_identite = fields.Boolean(compute='_compute_field_visibility')
+
+
--- chichcorner_customization/static/src/app/screens/receipt_screen/receipt/receipt_header/models_stan.js
+++ chichcorner_customization/static/src/app/screens/receipt_screen/receipt/receipt_header/models_stan.js
@@ -9,12 +9,14 @@ patch(Order.prototype, {
         this.identite_number = this.identite_number || null;
         this.cheque_number = this.cheque_number || null;
         this.banque = this.banque || null;
+        this.cheque_date = this.cheque_date || null;
     },
     init_from_JSON(json) {
         this.set_stan(json.stan);
         this.set_identite_number(json.identite_number);
         this.set_cheque_number(json.cheque_number);
         this.set_banque_name(json.banque);
+        this.set_cheque_date(json.cheque_date);
         super.init_from_JSON(...arguments);
     },
     export_as_JSON() {
@@ -24,11 +26,18 @@ patch(Order.prototype, {
             json.identite_number = this.get_identite_number();
             json.cheque_number = this.get_cheque_number();
             json.banque = this.get_banque_name();
+            json.cheque_date = this.get_cheque_date();
         }
         return json;
     },
     set_stan(stan) {
         this.stan = stan;
+    },
+    set_cheque_date(cheque_date) {
+        this.cheque_date = cheque_date;
+    },
+     get_cheque_date() {
+        return this.cheque_date;
     },
     get_stan() {
         return this.stan;
